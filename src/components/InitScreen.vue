cde967b71b36fe58c3f7e005045ed6db54fe5eba
Roman Alyakritskiy <rawork@yandex.ru>
2017-12-22 00:14:04 +0300

    vue-spinner

--- src/components/InitScreen.vue
+++ src/components/InitScreen.vue
@@ -1,5 +1,16 @@
 <template>
-    <h2 class="init-message">Игра загружается, подождите...</h2>
+    <div style="text-align: center">
+        <h2 class="init-message">Игра загружается, подождите...</h2>
+        <spinner :size="40" :depth="2" color="#f1f1f1" :speed="1.0"></spinner>
+    </div>
 </template>
 
-<script></script>
+<script>
+  import Spinner from 'vue-spinner-component/src/Spinner.vue'
+
+  export default {
+    components: {
+      Spinner
+    }
+  }
+</script>
